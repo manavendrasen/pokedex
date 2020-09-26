96b1cbd41e8d680e9ee4f1f3d443a37c9c2cbbad
manavendrasen <manavendra.199302058@muj.manipal.edu>
2020-09-26 19:09:28 +0530

    basic details api fetching and display

--- src/views/PokemonDetails.jsx
+++ src/views/PokemonDetails.jsx
@@ -1,12 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "../axios/axios";
 
-const PokemonDetails = (props) => {
-  const { match } = props;
-  const { params } = match;
-  const { pokemonIndex } = params;
+import requests from "../requests/request";
+
+const PokemonDetails = ({ match }) => {
+  const { pokemonIndex } = match.params;
+  const url = requests.fetchPokemon;
+  const [pokemon, setPokemon] = useState({});
+
+  useEffect(() => {
+    const fetchDetails = async () => {
+      const response = await axios.get(url + `/${pokemonIndex}`);
+      setPokemon(response.data);
+    };
+    fetchDetails();
+  }, [url, pokemonIndex]);
+
+  const getDetails = (pokemon) => {
+    //Destructuring the data
+    const { name, height, weight, types, stats } = pokemon;
+    console.log(stats);
+    return (
+      <div>
+        {name}
+        <div>{height}</div>
+        <div>{weight}</div>
+        {/* {stats.map((stat, index) => {
+          const typeName = stat.base_stat;
+          return <div>{typeName}</div>;
+        })} */}
+      </div>
+    );
+  };
+
   return (
     <div>
-      <h1>Details for {pokemonIndex}</h1>
+      <h1>{getDetails(pokemon)}</h1>
     </div>
   );
 };
