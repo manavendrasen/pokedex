8e9a6c48579e920dda44d7f40051bbf91cd63ad4
manavendrasen <manavendra.199302058@muj.manipal.edu>
2020-10-06 12:46:12 +0530

    added search feature

--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 //components
 import { Search } from "../components/Search";
 import { PokemonList } from "../components/pokemon/PokemonList";
-
 //data
 import requests from "../requests/request";
+
+//handleSearch
+
 const Dashboard = () => {
+  const [filter, setFilter] = useState("");
+  const handleSearch = (e) => {
+    let val = e.target.value;
+    val = val.toLowerCase();
+    setFilter(val);
+  };
+
   return (
-    <div>
-      <Search />
-      <PokemonList fetchUrl={requests.fetchPokemon} />
-    </div>
+    <>
+      <Search handleSearch={handleSearch} />
+      <PokemonList filter={filter} fetchUrl={requests.fetchPokemon} />
+    </>
   );
 };
 
